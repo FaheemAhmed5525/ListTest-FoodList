6f21e4f3a567182cc614954e1d9f7b33ba0b906b
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-09-09 11:34:53 +0500

    View test commit

--- FoodListView/ContentView.swift
+++ FoodListView/ContentView.swift
@@ -8,17 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    let foodItems = [
+        FoodItem(name: "Apple", totalCalories: 76.2, image: "Apple"),
+        FoodItem(name: "Banana", totalCalories: 102.0, image: "Banana"),
+        FoodItem(name: "Mango", totalCalories: 138.5, image: "Mango"),
+        FoodItem(name: "Orange", totalCalories: 85.0, image: "Orange")]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationStack {
+                List(foodItems) { item in
+                    ItemCellView(item: item)
+                }
+                .navigationTitle("Fruits")
+            }
         }
-        .padding()
+        .padding(0)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+struct ItemCellView: View {
+    
+    let item : FoodItem
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            Image(item.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .frame(width: 102, height: 70)
+            VStack(alignment: .leading) {
+                Text(item.name)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.green)
+                Text("Average Calories: \(item.totalCalories.formatted())")
+            }
+            .padding(0)
+        }
+        .padding(8)
+    }
 }
--- FoodListView/FoodItem.swift
+++ FoodListView/FoodItem.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class FoodItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let totalCalories: Float
+    let image : String
+    
+    public init(name: String, totalCalories: Float, image: String) {
+        self.name = name
+        self.totalCalories = totalCalories
+        self.image = image
+    }
+}
