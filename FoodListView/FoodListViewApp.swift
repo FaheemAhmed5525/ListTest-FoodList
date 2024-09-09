6730f235d2b8aad59803a5e2ce9aba0090d0ba61
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-09-09 10:33:25 +0500

    Initial Commit

---
+++ FoodListView/FoodListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodListViewApp.swift
+//  FoodListView
+//
+//  Created by Faheeam Ahmed on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
